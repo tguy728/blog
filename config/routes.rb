40a6c1fb69656bf36fb9eb58c3538793c41c0dc4
Trey Guy <tguy728@gmail.com>
2019-12-09 03:55:30 +0000

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ get "index", to: "articles#index"
 
   resources :articles
 
-  root to: "pages#homes"
+  root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
